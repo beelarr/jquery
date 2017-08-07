0887479b606d3e58dc559b65905fb0760f2e389e
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-07 14:05:34 -0500

    finished

--- javascripts/main.js
+++ javascripts/main.js
@@ -27,11 +27,87 @@ $(function () {
     $.ajax({
         url: 'javascripts/songs.json'
     }).done(functionIWantJQueryToExecute)
+        .fail(function (error) {
+            console.log(error)
 
-    function functionIWantJQueryToExecute() {
+        })
+
+    // $.ajax({
+    //     url: 'javascripts/songs.json'
+    //     success:
+    //     fail:
+    //
+    // })
+
+        })
+    
+
+
+    function functionIWantJQueryToExecute(songList) {
         console.log(songList.songs)
+
+        for (let i = 0; i < songList.songs.length; i++){
+            let currentSong = songList.songs[i];
+            $('#list-of-songs').append(`<h1> ${currentSong.title}</h1>
+                <div>Performed by ${currentSong.artist}<div>${currentSong.album}`)
+
+
+                        
+
+        }
 
     }
 
-    
+let howmanyBeans = 6
+let howmanyCows = 2
+
+$('#resultBtn').click(()=>{
+    resultFunction(howmanyBeans, howmanyCows);
+
 });
+
+function resultFunction(a, b) {
+    let result = a * b
+
+    $('#result-1').html(`${result} random items of cows and beans`)
+}
+
+$('#scaryBtn').click(()=>{
+    $('li').toggle()
+    console.log($('li').is(':hidden'))
+    $('li').is(':hidden') ? $('#scaryBtn').html('Show') : $('#scaryBtn').html('Too Scary')
+})
+
+let storyItems = $(".story")
+for (let i = 0; i < storyItems.length; i++){
+    storyItems[i].onmouseover = over;
+    storyItems[i].onmouseout = out;
+
+function over() {
+     console.log(this.id, event.currentTarget, event.target)
+    this.style.backgroundColor = 'peachpuff';
+}
+
+function out() {
+    this.style.backgroundColor = '#ffffff';
+}
+
+}
+
+
+$('.inner').append(`, <strong>Jack</strong>`)
+
+
+$('#holiday').click(()=>{
+    doSomething($("#holiday"))
+})
+
+function doSomething(obj) {
+    console.log(obj, event)
+    event.target.style.backgroundColor = 'red'
+}
+
+$("#tomorrow").click((event)=>{
+    doSomething(event)
+});
+
